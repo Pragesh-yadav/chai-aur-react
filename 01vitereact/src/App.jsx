517aeb81d4bc449aa13fabfbefb7a23e9074a1be
pragesh-yadav <drop2pragesh@gmail.com>
2025-06-29 19:25:02 +0530

    Back ground changer in react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+
+  return (
+    <>
+    <Chai />
+    <h1>React | Vite </h1>
+    <p>Using fragment to return single </p>
+
+    </>
+      )
+}
+
+export default App
